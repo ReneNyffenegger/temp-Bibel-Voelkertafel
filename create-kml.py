7364562fd1b2844bc9c2423f8245ca94d9f1360e
René Nyffenegger <mail@renenyffenegger.ch>
2018-10-26 03:12:26 +0200

    move portion to temp-create...KMLs

--- create-kml.py
+++ create-kml.py
@@ -1,44 +1,53 @@
 #!/bin/python
 
-class Coord:
-    def __init__(self, lat, lon):
-        self.lat = lat
-        self.lon = lon
+#q
+#q       Gone to github / temp-create-Google-Earth-KMLs
+#q
+#q class Coord:
+#q     def __init__(self, lat, lon):
+#q         self.lat = lat
+#q         self.lon = lon
+#q 
+#q 
+#q 
+#q def start_folder(kml, name):
+#q     kml.write('<Folder><name>{:s}</name><open>1</open>'.format(name))
+#q 
+#q def end_folder(kml):
+#q     kml.write('</Folder>')
+#q 
+#q def pushpin(kml, name, coord, desc):
+#q     kml.write('<Placemark><name>{:s}</name>'.format(name))
+#q 
+#q     if desc is not None:
+#q        kml.write('<description>{:s}</description>'.format(desc))
+#q 
+#q     kml.write("""
+#q <!--   <LookAt>
+#q 					<longitude>38.34803907965529</longitude>
+#q 					<latitude>33.2540199398231</latitude>
+#q 					<altitude>0</altitude>
+#q 					<heading>11.08435171895972</heading>
+#q 					<tilt>1.703726260245887</tilt>
+#q 					<range>4693076.838697146</range>
+#q 					<gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
+#q 				</LookAt>-->
+#q 				<styleUrl>#m_ylw-pushpin</styleUrl>
+#q 				<Point>
+#q 					<gx:drawOrder>1</gx:drawOrder>
+#q                                         <coordinates>{:f},{:f}</coordinates>
+#q 				</Point>
+#q 			</Placemark>""".format(coord.lon, coord.lat)
+#q     )
+#q 
+#q 
+#q 
+#q 
 
+import sys
+sys.path.insert(0, '/var/tmp/temp-create-Google-Earth-KMLs')
 
-
-def start_folder(kml, name):
-    kml.write('<Folder><name>{:s}</name><open>1</open>'.format(name))
-
-def end_folder(kml):
-    kml.write('</Folder>')
-
-def pushpin(kml, name, coord, desc):
-    kml.write('<Placemark><name>{:s}</name>'.format(name))
-
-    if desc is not None:
-       kml.write('<description>{:s}</description>'.format(desc))
-
-    kml.write("""
-<!--   <LookAt>
-					<longitude>38.34803907965529</longitude>
-					<latitude>33.2540199398231</latitude>
-					<altitude>0</altitude>
-					<heading>11.08435171895972</heading>
-					<tilt>1.703726260245887</tilt>
-					<range>4693076.838697146</range>
-					<gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode>
-				</LookAt>-->
-				<styleUrl>#m_ylw-pushpin</styleUrl>
-				<Point>
-					<gx:drawOrder>1</gx:drawOrder>
-                                        <coordinates>{:f},{:f}</coordinates>
-				</Point>
-			</Placemark>""".format(coord.lon, coord.lat)
-    )
-
-
-
+from createKMLs import *
 
 
 class Person:
